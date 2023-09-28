3c0a5c82eff130a2ca86a6357973802caab2ded7
Daiki Iijima <daiki.applecreate@gmail.com>
2023-09-28 22:31:52 +0900

    Initial Commit

---
+++ MysteryMealRoulette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MysteryMealRoulette
+//
+//  Created by 飯島大樹 on 2023/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
